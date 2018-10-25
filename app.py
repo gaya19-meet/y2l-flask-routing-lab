254e6a7b9663cd70f54f937fc40aafd3457a0bc3
sivan18-meet <sbmislovely@gmail.com>
2018-10-25 18:55:20 +0300

    i didnt finish part 3

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+	pets_list = "pets list"#["cat" , "dog" , "unicorn" , "panda" , "bear"]
+	return render_template("home.html" , pets=pets_list)
+
+
+@app.route('/shop')
+def shop_page():
+    return render_template("shop.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
+
